286dd089ba7ae68afd66c5cba5c818f43b941630
Rui Carneiro <rui.motacarneiro@heig-vd.ch>
2023-03-15 14:35:40 +0100

    Singleton Display

--- src/Display.java
+++ src/Display.java
@@ -3,6 +3,15 @@ import java.awt.Graphics2D;
 
 public class Display extends JFrame implements Displayer {
 
+    private static Display instance;
+
+    private Display() {
+    }
+
+    public static Display getInstance() {
+        if (instance == null) instance = new Display();
+        return instance;
+    }
 
     @Override
     public int getWidth() {
