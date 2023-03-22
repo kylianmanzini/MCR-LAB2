d961df34d23253487a4220abae5226d343492977
Rui Carneiro <rui.motacarneiro@heig-vd.ch>
2023-03-22 15:39:22 +0100

    FPS

--- src/Bouncers.java
+++ src/Bouncers.java
@@ -8,9 +8,25 @@ public class Bouncers {
         /* … */
     }
     public void run() {
-        JFrame frame = new JFrame();
-        frame.setSize(1000,1000);
-        frame.setVisible(true);
+        int fps = 25;
+        long lastTime, currentTime;
+        lastTime = System.currentTimeMillis() - 40;
+        while (true) {
+            currentTime = System.currentTimeMillis();
+            try {
+                Thread.sleep(Math.max((1000 / fps) - (currentTime - lastTime), 0));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            lastTime = System.currentTimeMillis();
+
+            // TODO : Whatever you want
+            /*
+            JFrame frame = new JFrame();
+            frame.setSize(1000, 1000);
+            frame.setVisible(true);
+            */
+        }
     }
     public static void main(String ... args) {
         new Bouncers().run();
